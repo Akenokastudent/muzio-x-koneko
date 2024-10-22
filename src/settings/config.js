f60245ee836b014d0aba2c3ec49eb6ede510de4f
Akenokastudent <neerusingh671972@gmail.com>
2024-10-22 19:40:57 +0530

    Update config.js

--- src/settings/config.js
+++ src/settings/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-    token: process.env.TOKEN || "BOTTOKEN", // <==== PASTE YOU TOKEN
-    prefix: process.env.PREFIX || "f?", // <==== SET YOU PRERIX BOT [ OWNER COMMANDS ]
+    token: process.env.TOKEN || "MTI5NzkzNzA4OTM1MDA3ODU5Nw.GNKPxp.1j9XXqze2R1xgRNExeRvFSniDodK-0GlfuTmoA", // <==== PASTE YOU TOKEN
+    prefix: process.env.PREFIX || "!", // <==== SET YOU PRERIX BOT [ OWNER COMMANDS ]
     color: process.env.EMBED_COLOR || "#c50632", // <==== YOU EMBEDED HEX COLOR
-    owner: process.env.OWNER_ID || "900708551805268019", // <==== BOTS OWNER ID
+    owner: process.env.OWNER_ID || "\1297206086830522398", // <==== BOTS OWNER ID
     guildLogs: process.env.GUILD_LOGS || "000000000000000", // <==== YOUR SERVER JOIN LEFT LOGS CHANNEL ID
     leaveTimeout: process.env.LEAVE_TIMEOUT || "20000", // <==== SET LEAVE TIMEOUT WHEN BOT WAS ALONE || 1000 = 1sec
     disableYouTube: parseBoolean(process.env.DISABLE_YOUTUBE || "false"), // <==== SET "TRUE OR FALSE" | ENABLE/DISABLE YOUTUBE FEATURES. DISABLING THIS WILL MAKE "AUTOPLAY" COMMANDS USELESS!!!
@@ -15,7 +15,7 @@ module.exports = {
         defaultPlatform: process.env.DEFAULT_SOURCE || "ytsearch", // <==== SET DEFAULT SOURCE || "ytsearch","ytmsearch","scsearch"
         clientID: process.env.SPOTIFY_ID || "YourSpotifyID", // <==== SPOTIFY CLIENT ID
         clientSecret: process.env.SPOTIFY_SECRET || "YourSpotifySecret", // <==== SPOTIFY CLIENT SECRET
-        reconnectTries: 5, // <==== TOTAL ATTEMPS TO TRY IF RECONNECT FAILED. YOU CAN CHANGE IT TO "Infinity" FOR UNLIMITED ATTEMPS.
+        reconnectTries: Infinity, // <==== TOTAL ATTEMPS TO TRY IF RECONNECT FAILED. YOU CAN CHANGE IT TO "Infinity" FOR UNLIMITED ATTEMPS.
         playlistLimit: 2, // <==== 1 = 100 TRACKS
         albumLimit: 2, // <==== 1 = 50 TRACKS
         artistLimit: 2, // <==== 1 = 50 TRACKS
@@ -23,10 +23,10 @@ module.exports = {
     },
     nodes: [
         {
-            name: process.env.NODE_NAME1 || "Node 1", // lavalink node name (anything you want)
-            host: process.env.NODE_HOST1 || "lavalink.mlserv.xyz", // <==== LAVALINK NAME HOST
-            port: parseInt(process.env.NODE_PORT1 || "32669"), // <==== LAVALINK PORT
-            password: process.env.NODE_PASSWORD1 || "dsc.gg/sciencegear", // <==== PASSWORD/AUTH
+            name: process.env.NODE_NAME1 || "Node 0", // lavalink node name (anything you want)
+            host: process.env.NODE_HOST1 || " node.lewdhutao.my.eu.org", // <==== LAVALINK NAME HOST
+            port: parseInt(process.env.NODE_PORT1 || "80"), // <==== LAVALINK PORT
+            password: process.env.NODE_PASSWORD1 || "youshallnotpass", // <==== PASSWORD/AUTH
             secure: parseBoolean(process.env.NODE_SECURE1 || "false"), // <==== LAVALINK SECURE "true/false"
             regions: process.env.NODE_REGIONS1 || ["singapore"], // <==== AVAILABLE LAVALINK REGIONS [ "singapore","indonesian","japan"," ]
         },
@@ -41,7 +41,7 @@ module.exports = {
     ],
 
     // ⬇⬇⬇ LINK BOTS DETAILS
-    mongoUri: process.env.MONGO_URI || "mongodb+srv://YourMongoURI", // <==== YOUR MONGODB LINK
+    mongoUri: process.env.MONGO_URI || "mongodb+srv://daadsafaff:@cluster0.c0srf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", // <==== YOUR MONGODB LINK
     supportUrl: process.env.SUPPORT_URL || "https://discord.gg/TjtXnYD9SP", // <==== YOUR SUPPORT SERVER LINK
     voteUrl: process.env.VOTE_URL || "https://www.gearhost.my.id/", // <==== YOUR VOTE URL
     inviteUrl: process.env.INVITE_URL || "https://discord.com/api/oauth2/authorize?client_id=1122883732303511583&permissions=8&scope=bot+applications.commands", // <==== YOUR BOT INVITE LINK
